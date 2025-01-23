3b31335f197826e3fae0d4ed73e0e36e5f6935c1
Szalapbp <szalapbp@mail.uc.edu>
2025-01-23 13:06:25 -0500

    All java files added

---
+++ src/src/PersonGenerator.java
@@ -0,0 +1,67 @@
+package src;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonGenerator {
+    public static void main(String[] args)
+    {
+        boolean doneInput = false;
+
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        String rec ="";
+        int YOB = 0;
+        ArrayList<String> people = new ArrayList<String>();
+
+
+        Scanner in = new Scanner(System.in);
+
+        do{
+            ID = SafeInput.getNonZeroLenString(in, "Enter your ID [######]");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+            title = SafeInput.getNonZeroLenString(in, "Enter your title");
+            YOB = SafeInput.getRangedInt(in, "Enter your year of birth", 1000, 9999);
+
+            rec = (ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB);
+            System.out.println(rec);
+
+            people.add(rec);
+
+            doneInput = SafeInput.getYNConfirm(in,"Are you done? [Y/N]");
+
+        }while(!doneInput);
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
+        try {
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            for(String person : people)
+            {
+                writer.write(person, 0, person.length());
+                writer.newLine();
+            }
+
+            writer.close();
+            System.out.println("Data file written!");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
